72ccaa1ef5777430ce293a9b69ce074c1c0dd6f2
Janarthanan003 <ajanarthanan06@gmail.com>
2023-11-16 10:37:45 +0530

    first commit

---
+++ src/comparesums/CompareSums.java
@@ -0,0 +1,16 @@
+package comparesums;
+
+public class CompareSums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        int a = 10;
+		        int b = 20;
+		        int c = 5;
+		        int d = 15;
+		        
+		        if (a + b > c + d) {
+		            System.out.println("The sum of a and b is greater than the sum of c and d.");
+		        }
+		    }
+}
